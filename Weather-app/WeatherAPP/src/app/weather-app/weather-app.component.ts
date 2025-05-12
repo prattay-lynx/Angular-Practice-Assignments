f744e66f69dceb59e4168131e34e95675086e02c
Prattay <prattaypaul2@gmail.com>
2025-05-12 19:44:05 +0530

    update

---
+++ Weather-app/WeatherAPP/src/app/weather-app/weather-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-app',
+  imports: [],
+  templateUrl: './weather-app.component.html',
+  styleUrl: './weather-app.component.css'
+})
+export class WeatherAppComponent {
+
+}
